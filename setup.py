f756f3ba1faf606b79232104c5e71cbb1d586a7b
nacef <aziznacef2@gmail.com>
2021-08-12 02:18:03 +0200

    Scratched initial auth, redundant

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ import psycopg2
 
 
 
-def setupdb(dbcurr,conn):
-    
+def setupdb(dbcurr,conn): 
+    queries = ["CREATE TABLE platforms(name varchar(20) PRIMARY KEY);", "CREATE TABLE passwords(id INT PRIMARY KEY, val varchar(50), plt varchar(20) REFERENCES platforms(name) ON DELETE CASCADE);"]
     dbcurr.execute("""
         SELECT EXISTS(
             SELECT * FROM pg_tables
             where schemaname = 'public'
         );""")
     if(not dbcurr.fetchone()[0]):    
-        try:    
-            dbcurr.execute("CREATE TABLE platforms(name varchar(20) PRIMARY KEY);")
-            conn.commit()
-            dbcurr.execute("CREATE TABLE passwords(id INT PRIMARY KEY, val varchar(50), plt varchar(20) REFERENCES platforms(name) ON DELETE CASCADE);")
+        try:
+            for query in queries:
+                dbcurr.execute(query)
             conn.commit()
             print("Tables created.")
         except:
@@ -23,21 +22,15 @@ def setupdb(dbcurr,conn):
             cur.close()
 
 
-print("Welcome to the password manager setup process.")
-#Connect to an existing db
-try:
-    conn = psycopg2.connect("dbname=PwdManager user=postgres")
-    cur = conn.cursor()
-    print("Connected.")
-except:
-    print("Connection failed!")
 
-setupdb(cur,conn)
+if __name__ == '__main__':
+    print("Welcome to the password manager setup process.")
+    #Connect to an existing db
+    try:
+        conn = psycopg2.connect("dbname=PwdManager user=postgres")
+        cur = conn.cursor()
+        print("Connected.")
+    except:
+        print("Connection failed!")
 
-passphrase = input("Please enter a passphrase, this will be used to enter the manager and will determine how data is encrypted.\n")
-os.environ.update({"Pmngr_pwd":passphrase})
-
-print("Testing env write:\n")
-print(os.getenv("Pmngr_pwd"))
-#conn.close()
-#cur.close()
+    setupdb(cur,conn)
